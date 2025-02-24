7a6d7653005538e392a8c763565873b34130d6d3
GinaKoul <129206273+GinaKoul@users.noreply.github.com>
2025-02-24 23:28:48 +0200

    Load Images

--- example/src/example.js
+++ example/src/example.js
@@ -1,4 +1,80 @@
-import "menu-drop/dist/menu-drop.css";
-import { MenuDrop } from "menu-drop/dist/menu-drop.js";
+import "./css/example.css";
 
-console.log(MenuDrop.getBreakpointSm());
+const ImageCarousel = (function (doc) {
+  let ImgCarousels;
+
+  function initCarousel(carousel) {
+    const carouselSlides = carousel.childNodes;
+    for (const node of carouselSlides) {
+      console.log(node);
+    }
+    // carouselSlides.forEach((slide, index) => {
+    //   console.log(slide);
+    //   slide.setAttribute("data-id", index);
+    //   if (index === 0) slide.classList.add("active");
+    // });
+  }
+
+  function initCarousels() {
+    ImgCarousels = doc.querySelectorAll(".img-carousel");
+    ImgCarousels.forEach((carousel) => {
+      initCarousel(carousel);
+    });
+  }
+  return {
+    init: initCarousels,
+  };
+})(document);
+
+(function (doc) {
+  let carousel;
+
+  function createImage(images) {
+    const newImage = doc.createElement("img");
+    newImage.setAttribute("srcset", images.srcSet);
+    newImage.setAttribute("src", images.src);
+    newImage.setAttribute("alt", images.placeholder);
+    newImage.setAttribute("sizes", "(max-width: 676px) 500px, 1920px");
+    return newImage;
+  }
+
+  function createSlide(images) {
+    let slide = doc.createElement("div");
+    slide.classList.add("img-slide");
+    slide.append(createImage(images));
+    carousel.append(slide);
+  }
+
+  function addImages() {
+    import(`./images/appetizers.jpg?sizes[]=500,sizes[]=1920`).then(
+      (images) => {
+        createSlide(images);
+      },
+    );
+
+    import(`./images/desserts.jpg?sizes[]=500,sizes[]=1920`).then((images) => {
+      createSlide(images);
+    });
+    import(`./images/pasta.jpg?sizes[]=500,sizes[]=1920`).then((images) => {
+      createSlide(images);
+    });
+    import(`./images/pizza.jpg?sizes[]=500,sizes[]=1920`).then((images) => {
+      createSlide(images);
+    });
+    import(`./images/salad.jpg?sizes[]=500,sizes[]=1920`).then((images) => {
+      createSlide(images);
+    });
+  }
+
+  function init() {
+    carousel = doc.querySelector(".img-carousel");
+    addImages();
+    ImageCarousel.init();
+  }
+
+  if (doc.readyState === "loading") {
+    doc.addEventListener("DOMContentLoaded", init);
+  } else {
+    init();
+  }
+})(document);
--- example/webpack.common.js
+++ example/webpack.common.js
@@ -3,32 +3,55 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-    target: "web",
-    entry: "./src/example.js",
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "example.css",
-        }),
-        new HtmlWebpackPlugin({
-            template: "./template.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+  target: "web",
+  entry: "./src/example.js",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "example.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: "responsive-loader",
+            options: {
+              sizes: [1920, 767],
+              quality: 85,
+              placeholder: true,
+              outputPath: "dist/images/",
+              format: "webp",
+              // If you want to enable sharp support:
+              adapter: require("responsive-loader/sharp"),
+              additionalPaths: [
+                {
+                  width: 0,
+                  format: "webp",
+                  quality: 85,
+                  outputPath: "dist/images/",
+                },
+              ],
             },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },  
+          },
         ],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-        ],
-    },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
 };
